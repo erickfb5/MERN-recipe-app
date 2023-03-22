8d6d857d2a7548c37013358e5fd6aca6ff966b5b
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-03-22 16:59:08 -0300

    set up Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
+import {useCookies} from 'react-cookie'
+import {useNavigate} from 'react-router-dom'
+
 import { Form } from "./Form";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [_, setCookies] = useCookies(["access_token"])
+
+  const navigate = useNavigate()
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+
+
+setCookies('access_token', response.data.token)
+window.localStorage.setItem('userID', response.data.userID)    
+navigate('/')
+
+} catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Form
@@ -11,7 +36,8 @@ const Login = () => {
       setUsername={setUsername}
       password={password}
       setPassword={setPassword}
-      label='Login'
+      label="Login"
+      onSubmit={onSubmit}
     />
   );
 };
